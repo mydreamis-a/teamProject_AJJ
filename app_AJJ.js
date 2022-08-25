1e88d6bf72d9de9c0077a718108759aeaa1e5f10
acca3434 <wnqudgus1234@gmail.com>
2022-08-25 18:48:14 +0900

    드디어 첫경험

--- app_AJJ.js
+++ app_AJJ.js
@@ -12,7 +12,8 @@ const jwt = require("jsonwebtoken");
 const socketio = require("socket.io");
 const dot = require("dotenv").config();
 const session = require("express-session");
-const { sequelize } = require("./model/index_AJJ");
+const cookieParser = require("cookie-parser");
+const { sequelize,User } = require("./model/index_AJJ");
 const FileStore = require("session-file-store")(session);
 const createProducts = require("./router/createProducts_AJJ");
 
@@ -26,7 +27,6 @@ const server = app.listen(PORT, () => {
 });
 const io = socketio(server);
 
-//
 // ㅜ body-parser
 app.use(express.urlencoded({ extended: false }));
 
@@ -47,18 +47,32 @@ app.use("/img", express.static(path.join(__dirname, "/img_Ahn_Ju")));
 // ㅜ 해당 요청 주소에 대해서 라우터 설정
 app.use("/example", example);
 
-app.use(
-  session({
-    secret: process.env.JU_SECRET_KEY,
-    //
-    // ㅜ 저장하고 불러올 때 다시 저장할 지 여부
-    resave: false,
-    //
-    // ㅜ 저장 시 초기화 여부
-    saveUninitialized: true,
-    store: new FileStore(),
-  })
-);
+
+// 220825 추가부분 주병현
+let accessToken = process.env.JU_ACCESS_TOKEN;
+let refreshToken = process.env.JU_REFRESH_TOKEN;
+
+// 220825 추가부분 주병현
+const sharedsession = require("express-socket.io-session");
+io.use(sharedsession(session({
+  secret: process.env.JU_SECRET_KEY,
+  resave: true,
+  saveUninitialized: true,
+  store : new FileStore(),
+})));
+
+// app.use(
+//   session({
+//     secret: process.env.JU_SECRET_KEY,
+//     //
+//     // ㅜ 저장하고 불러올 때 다시 저장할 지 여부
+//     resave: false,
+//     //
+//     // ㅜ 저장 시 초기화 여부
+//     saveUninitialized: true,
+//     store: new FileStore(),
+//   })
+// );
 
 // ㅜ 서버 실행 시 MySQL 연동
 sequelize
@@ -76,5 +90,69 @@ app.get("/", (req, res) => {
   createProducts();
   res.render("main_AJJ");
 });
+
+// 220825 추가부분 주병현
+io.on("connection", (socket) => {
+  socket.on("login",(userInfor) => {
+
+      const userInputEmail = userInfor.userInputEmail;
+      const userInputPw = userInfor.userInputPw;
+      const a = 0;
+      User.findAll({
+              where: {
+                  email: userInputEmail,
+                  password: userInputPw
+              }
+          }).then((user) => {
+          let aT = jwt.sign({
+              password : user[0].password,
+              type : "JWT",
+            },
+            accessToken,
+            {
+              issuer : "주병현",
+              expiresIn : "5m"
+            })
+          let rT = jwt.sign({
+              password : user[0].password,
+              type : "JWT",
+            },
+            refreshToken,
+            {
+              issuer : "주병현",                
+              expiresIn : "30m"
+            })
+            socket.handshake.session.aT = aT;
+            socket.handshake.session.save();
+            socket.handshake.cookies.rT = rT;
+              socket.emit("loginSuccess", user[0].name);
+          }).catch((e) => {
+              console.log(e);
+          })
+      })
+    socket.on("signUp",(inputName,inputTel,inputEmail,inputPassword)=>{
+      const inputNameData = inputName;
+      const inputTelData = inputTel;
+      const inputEmailData = inputEmail;
+      const inputPasswordData = inputPassword;
+    User.findOrCreate({
+      where : {
+        phone : inputTelData,
+        email : inputEmailData,
+      },
+      defaults: {
+        name : inputNameData,
+        phone : inputTelData,
+        email : inputEmailData,
+        password : inputPasswordData
+      },
+    }).then((e)=>{
+      console.log(e)
+      socket.emit("signSuccess",inputNameData);
+    }).catch((e)=>{
+      console.log(e);
+    })
+  })
+})
 
 // 08.25.10 수정
