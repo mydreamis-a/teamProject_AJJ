d83aeb0425ed48df1ff8482e7ee6ff81cd2f2df6
AnJuYeong <dksenfn7907@naver.com>
2022-09-05 10:19:16 +0900

    주말동안 한거

--- view/iconEventPage_AJJ.js
+++ view/iconEventPage_AJJ.js
@@ -7,15 +7,29 @@ let userPoint;
 // 쿠키 생성 함수
 let createCookie = function(name, value, time){
   let date = new Date();
-  date.setTime(date.getTime() + time * 60 * 1000);
+  date.setTime(date.getTime() + time * 5 * 1000);
   document.cookie = name + "=" + value + ";expries" + date.toUTCString() + ";path=/"
+};
+// 쿠키 유무
+let isActiveCookie = function(key){
+  // 값이 있는지 없는지 빈문자열이 아니면 값이 있는것.
+  return getCookie(key) != null ? true : false;
+};
+// 쿠키 값 가져오기
+let getCookie = function(name){
+  // 현재 저장된 쿠키중 name에 맞는 쿠키가 저장되어 있으면 
+  let value = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+  // 있으면 값을 내보낸다.
+  // 쿠키의 값이 있는 인덱스가 2번이라서 인덱스 값을 가져온다.
+  // console.log("cookie" + value[2]);
+  // console.log("cookie" + value[0]);
+  return value ? value[2] : null;
 };
 
 // 이미 dailyCheck_ajax.js에서 선언해서 사용하기 때문에 재선언할 필요가 없다.
 // ㅜ 로그인된 회원의 아이디(이메일) 임의 지정
 
 function time() {
-  // mainEvent.style.display = "block";
   let eventleft = Math.floor(Math.random() * 1851);
   let eventtop = Math.floor(Math.random() * 801);
   mainEvent.style.top = eventtop + "px";
@@ -34,7 +48,7 @@ function time() {
   }
 }
 
-let point = ["0", "0", "0", "10", "10", "10", "100", "1000", "10000", "100000"];
+let point = ["0", "0", "0", "0", "10", "10", "100", "1000", "10000", "10000"];
 
 mainEvent.addEventListener("click", () => {
   let eventPoint = Math.floor(Math.random() * 10);
@@ -50,12 +64,24 @@ mainEvent.addEventListener("click", () => {
     success: function (result) {
       if (result.data == "null") {
         alert(" 로그인하시면 포인트 받을 수 있음 ^^7 ");
+        time();
       } else {
         alert(result.data + "적립");
+        createCookie("asd","asd",1);
+        console.log(document.cookie);
+        let aa = isActiveCookie(result.name)
+        console.log(aa);
+        if(aa == true)
+        {
+          console.log("aa1");
+          mainEvent.style.display = "none";
+        } else{
+          console.log("aa2");
+          time();
+        }
       }
     },
   });
-  time();
 });
 
 time();
